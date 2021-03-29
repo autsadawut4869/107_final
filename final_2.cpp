0f7eb30ee7acec4fe1076c4264970980c40d49e6
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-03-29 18:48:57 +0700

    คอมไพล์ผ่านแต่ได้ 50 คะแนนครับ

--- final_2.cpp
+++ final_2.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	int n,i,j,k=0,x,max,ch;
-	scanf("%d", &n);
-	int a[1][n],c[10]={0};
+	long long int n,i,j,k=0,x,max,ch,posit=0,temp=0;
+	scanf("%lld", &n);
+	long long int a[1][n],c[10]={0};
 	for(i=0; i<1; i++){
 		for(j=0; j<n; j++){
-			scanf("%d", &a[i][j]);
+			scanf("%lld", &a[i][j]);
 		}
 	}
 	max=0;
@@ -23,7 +23,26 @@ int main()
 			continue;
 		}
 	}
-	printf("%d\n",a[0][ch]);
+	for(i=0; i<n; i++){
+		if(c[i]==max&&a[0][i]!=a[0][ch]){
+			posit=i;
+			temp=1;
+			break;
+		}
+	}
+	if(temp==1){
+		if(a[0][ch]>=a[0][posit])
+		{
+			printf("%lld %lld\n",a[0][posit],a[0][ch]);	
+		}
+		else
+		{
+			printf("%lld %lld\n",a[0][ch],a[0][posit]);	
+		}
+	}else{
+		printf("%lld\n",a[0][ch]);
+	}
+	
 	
 	return 0;
 }
